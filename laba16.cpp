05ad2b19f9648ab864be7573a278be5b1fd60c25
KondratenkoDanil <129153766+KondratenkoDanil@users.noreply.github.com>
2024-12-17 19:41:29 +0200

    Create laba16.cpp

---
+++ laba16.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double x = 5.0; // Задайте значення x (замініть на потрібне)
+    double numerator, denominator, y;
+
+    // Обчислення чисельника: 3000 + x
+    numerator = 3000 + x;
+
+    // Обчислення знаменника: x^2 + 2.6 * x - 7.5
+    denominator = x * x + 2.6 * x - 7.5;
+
+    // Перевірка ділення на нуль
+    if (denominator != 0) {
+        y = numerator / denominator;
+        cout << "Результат: y = " << y << endl;
+    }
+    else {
+        cout << "Помилка: ділення на нуль." << endl;
+    }
+
+    return 0;
+}
